3a243249c4df880c4234991ef47a26a589468e0c
yash <katukotayaswanth@gmail.com>
2021-07-31 12:32:06 +0530

    modified employee class

--- ToDoListGit/src/com/mph/model/Employee.java
+++ ToDoListGit/src/com/mph/model/Employee.java
@@ -2,4 +2,36 @@ package com.mph.model;
 
 public class Employee {
 
+	private int empno;
+	private String empname;
+
+
+	public Employee(int empno, String empname) {
+		super();
+		this.empno = empno;
+		this.empname = empname;
+		
+	}
+
+	public int getEmpno() {
+		return empno;
+	}
+
+	public void setEmpno(int empno) {
+		this.empno = empno;
+	}
+
+	public String getEmpname() {
+		return empname;
+	}
+
+	public void setEmpname(String empname) {
+		this.empname = empname;
+	}
+
+	@Override
+	public String toString() {
+		return "Employee [empno=" + empno + ", empname=" + empname + "]";
+	}	
+
 }
